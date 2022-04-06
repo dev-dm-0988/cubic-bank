efc75db3096e310a7d0394a223a67d42a68444a1
durga.maharjan <durgamaharjan2011@gmail.com>
2022-04-06 19:56:22 -0400

    base project

---
+++ banking-service/src/main/java/com/rab3tech/customer/service/CustomerService.java
@@ -0,0 +1,12 @@
+package com.rab3tech.customer.service;
+
+import com.rab3tech.vo.CustomerVO;
+
+public interface CustomerService {
+
+	CustomerVO createAccount(CustomerVO customerVO);
+	
+	CustomerVO getProfile(String email);
+	
+
+}
